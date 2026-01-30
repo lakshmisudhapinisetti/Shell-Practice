ff98b4c6632b895781ef78848d183792ba48b9d4
Lakshmi Sudha Pinisetti <lakshmisudhapinisetti@ibm.com>
2026-01-30 19:58:09 +0530

    adding new things

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:: Hi Sudha ! How are you ?"
+echo "$person2:: Hi Nanii ! Iam good . How about you ?"
+echo "$person1:: I am fine ! "
+echo "$person2:: that's good to hear ! "
