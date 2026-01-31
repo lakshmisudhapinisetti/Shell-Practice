8df5abd8c926a3043dc66b935d85f60d20250f7b
sudha <sudhasri066@gmail.com>
2026-01-31 15:39:39 +0530

    successfully executed

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -12,6 +12,19 @@ if [ $Num -gt 20 ]; then
 elif [ $Num -eq 20 ]; then
     echo "given number $Num is equal to 20"
 else
-     echo " given number $Num is less than 20"
+     echo "  given number $Num is less than 20"
 fi
+
+sleep 10
+
+Number=$n
+
+echo "enter a number : $n "
+if [ $n % 2 -eq 0 ]
+     echo " $n is even number "
+else
+    echo "$n is odd number"
+fi      
+
+
      
