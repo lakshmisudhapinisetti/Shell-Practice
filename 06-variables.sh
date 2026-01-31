9fcbfe84fa2c3220a178555f569deb15aee362ca
sudha <sudhasri066@gmail.com>
2026-01-31 12:49:06 +0530

    successfully executed

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-Start_time=$(date + %s)
+Start_time=$(date +%s)
 
 echo "script executed at : $Start_time"
 
 sleep 10
 
-end_time=$(date + %s)
+end_time=$(date +%s)
 total_time=$(($end_time-$Start_time))
 echo " time = $total_time"
 
