6653c265d6493cf0ad8e0b0b121abe68757afbbe
sudha <sudhasri066@gmail.com>
2026-02-01 10:34:00 +0530

    successfully executed

--- 10-installation.sh
+++ 10-installation.sh
@@ -15,3 +15,19 @@ if [ $? -ne 0 ]; then
 else
     echo "Installing Nginx...SUCCESS"
 fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo " Installing MySQL ...Failure"
+else
+    echo "Installing MySQL...SUCCESS"
+fi
+
+dnf install nodejs -y
+
+if [ $? -ne 0 ]; then
+    echo " Installing nodejs ...Failure"
+else
+    echo "Installing nodejs...SUCCESS"
+fi
