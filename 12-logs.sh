38815f4e3a6de2892630097ebbbe9cadc3c4881c
sudha <sudhasri066@gmail.com>
2026-02-01 10:58:13 +0530

    successfully executed

---
+++ 12-logs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+     echo " please execuete with sudo access"
+     exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo " $2 ...Failure"
+    else
+        echo "$2...SUCCESS"
+    fi
+}
+
+dnf install nginx -y &>> $LOGS_FILE
+VALIDATE $? "Installing Nginx"
+
+dnf install mysql -y &>> $LOGS_FILE
+VALIDATE $? "Installing Mysql"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Installing nodejs"
