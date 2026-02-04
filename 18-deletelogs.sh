288ae3e1242d68b6234f6f54a11ed7f5c0c0c2d0
Lakshmi Sudha Pinisetti <lakshmisudhapinisetti@ibm.com>
2026-02-04 17:44:17 +0530

    adding new things

---
+++ 18-deletelogs.sh
@@ -0,0 +1,18 @@
+#!/bash/bin
+
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+B='\e[34m'
+N='\e[0m'
+
+LOGS_DIR=/home/ec2-user/app-logs
+LOGS_FILE="$LOGS_DIR/0.log"
+
+if [ ! -d $LOGS_DIR ]; then
+    echo -e "$LOGS_DIR does not exitst"
+    exit 1
+fi
+
+FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
+echo "$FILES_TO_DELETE"
